8bb6b82cd246ab3c05dd3bd64786c96c30bb2e39
Caleb <coffincw@gmail.com>
2017-09-09 10:16:33 -0400

    reorganized and completed jsoup learning program

--- src/WebReader.java
+++ src/WebReader.java
@@ -11,32 +11,40 @@ public class WebReader {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+	
 		
+		String webpage = "http://people.cs.umass.edu/~liberato/courses/2017-fall-compsci190d/";
+		Document doc = Jsoup.connect(webpage).get();
+	
 		
-		Document doc = Jsoup.connect("http://people.cs.umass.edu/~liberato/courses/2017-fall-compsci190d/").get();
-		String title = doc.title();
-		String body = doc.body().text();
-		Elements links = doc.select("a[href]");
+		scrapeLinks(doc);
+		System.out.println(doc.html());
+		scrapeText(doc);
 		
+	}
+	
+	
+	static void scrapeLinks(Document page) {
+		
+		Elements links = page.select("a[href]");
 		for (Element link : links) {
 			
 			System.out.println("link : " + link.attr("href"));
 			System.out.println("text : " + link.text());
 			
 		}
+	}
+	
+	static void scrapeText(Document page) {
+		String title = page.title();
+		String body = page.body().text();
 		
 		System.out.println(title);
 		System.out.println();
-		//System.out.println(body);
-		String description = doc.select("meta[name=description]").first().attr("content");
+		System.out.println(body);
+		
+		String description = page.select("meta[name=description]").first().attr("content");
 		System.out.println("Description : " + description);
-		
-		//String keywords = doc.select("meta[name=keywords]").first().attr("content");
-		//System.out.println("Keywords : " + keywords);
-		
-		String html = Jsoup.connect("http://people.cs.umass.edu/~liberato/courses/2017-fall-compsci190d/").get().html();
-		
-		//System.out.println(html);
 		
 	}
 
